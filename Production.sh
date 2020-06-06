a5a31f08361f48eef738dcac4a18c600c6384733
rokaiamohammed <rokaia.mohamed111099@gmail.com>
2020-06-06 17:33:44 +0200

    any

--- Production.sh
+++ Production.sh
@@ -32,6 +32,7 @@ echo $! > .pidfile
 set +x
 
 echo 'Now...'
+echo $IP_ADDRESS
 echo 'Visit http://localhost:5000 to see your Node.js/React application in action.'
 echo '(This is why you specified the "args ''-p 5000:5000''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
